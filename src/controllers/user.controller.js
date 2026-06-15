f8e02879a5c127a04a5cc9918ce83559158b792f
Shivansh Ranjan <ranjanshivansh08@gmail.com>
2026-06-15 09:15:59 +0530

    Add refreshAccessToken controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,21 @@ import { ApiError } from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
 import { uploadOnCloudiary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+
+const generateAccessandRefreshToken=async(userId)=>{
+    try {
+        const user=await User.findById(userId)
+        const accessToken=user.generateAccessToken()
+        const refreshToken=user.generateRefreshToken()
+        user.refreshToken=refreshToken
+        await user.save({validateBeforeSave:false})
+        return {accessToken,refreshToken}
+    } catch (error) {
+        console.log(error)
+        throw new ApiError(500,"Something went wrong when genrating refresh and access tokens")
+    }
+}
 
 const registerUser=asyncHandler(async(req,res)=>{
    //get user details from frontend
@@ -21,16 +36,20 @@ const registerUser=asyncHandler(async(req,res)=>{
    ){
         throw new ApiError(400,"fullname is required")
    }
-    const existedUser=User.findOne({
+    const existedUser=await User.findOne({
         $or:[{email},{username}]
     })
 
     if(existedUser){
         throw new ApiError(409,"Username or email already exits")
     }
-
+    console.log("BODY:", req.body);
+    console.log("FILES:", req.files);
     const avatarLocalPath=req.files?.avatar[0]?.path
-    const coverImageLocalPath=req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
@@ -43,7 +62,7 @@ const registerUser=asyncHandler(async(req,res)=>{
         throw new ApiError(400,"Avatar file is required")
    }
 
-   const user=User.create({
+   const user=await User.create({
     fullName,
     avatar:avatar.url,
     coverImage:coverImage?.url||" ",
@@ -63,4 +82,98 @@ const registerUser=asyncHandler(async(req,res)=>{
    )
 })
 
-export {registerUser}
+const loginUser=asyncHandler(async(req,res)=>{
+    //req body-> data
+    //username or email exists
+    //find user
+    //password check
+    //access token and refresh token
+    //send cookie
+    console.log(req.body)
+    const {username,email,password}=req.body
+    if(!(username || email)){
+        throw new ApiError(400,"username or email is required")
+    }
+    const user=await User.findOne({
+        $or:[{username},{email}]
+    })
+    
+    if(!user){
+        throw new ApiError(404,"Username doesnot exist")
+    }
+
+    const isPasswordValid=await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(404,"Password is incorrect")
+    }
+
+    const {refreshToken,accessToken}=await generateAccessandRefreshToken(user._id)
+    
+    const loggedInUser=await User.findById(user._id).select("-password -refreshToken")
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    
+    return res.status(200).cookie("refreshToken",refreshToken,options).cookie("accessToken",accessToken,options)
+              .json(
+                new ApiResponse(200,
+                    {
+                        user:loggedInUser,accessToken,refreshToken
+                    },
+                    "User logged in Successfully"
+                )
+              )
+})
+
+const logOutUser=asyncHandler(async(req,res)=>{
+    const k=await User.findByIdAndUpdate(req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new ApiResponse(200,{},"User logged out succesfully"))
+})
+
+const refreshAccessToken=asyncHandler(async(req,res)=>{
+   const incomingRefreshToken= req.cookies.refreshToken || req.body.refreshToken
+   if(!incomingRefreshToken){
+        throw new ApiError(401,"Unauthorized request")
+   }
+
+   try {
+    const decodedToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+    const user=await User.findById(decodedToken?._id)
+    if(!user){
+     throw new ApiError(401,"Invalid Refresh Token")
+    }
+    if(incomingRefreshToken!==user?.refreshToken){
+     throw new ApiError(401,"Refresh Token is expired or used")
+    }
+ 
+    const options={
+     httpOnly:true,
+     secure:true
+    }
+    
+    const {accessToken,newRefreshToken}=await generateAccessandRefreshToken(user._id)
+    return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",newRefreshTokenefreshToken,options)
+    .json(
+     new ApiResponse(200,{accessToken,refreshToken:newRefreshToken},"Access Token Refreshed Successfully")
+    )
+   } catch (error) {
+    throw new ApiError(401,error?.message||"Invalid Refresh Token")
+   }
+})
+
+export {registerUser,loginUser,logOutUser,refreshAccessToken}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,26 +1,34 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-cloudinary.config({ 
+
+    
+const uploadOnCloudiary=async(localFilePath)=>{
+    try {
+        console.log("helllo")
+        if(!localFilePath) throw new Error("Couldnot find a path");
+        cloudinary.config({ 
         cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
         api_key:process.env.CLOUDINARY_API_KEY, 
         api_secret:process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
-
-const uploadOnCloudiary=async(localFilePath)=>{
-    try {
-        if(!localFilePath) throw new Error("Couldnot find a path");
         // uploading process
         const response=await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
         //file has been uploaded
-        console.log("File on cloudianry",response.url);
+        // console.log("File on cloudianry",response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
-        fs.unlinkSync(localFilePath)  //remove file locally as upload failed
-        return null;
+    console.error("Cloudinary Error:", error);
+
+    if(localFilePath && fs.existsSync(localFilePath)){
+        fs.unlinkSync(localFilePath);
     }
+
+    return null;
+}
 }
 
 export {uploadOnCloudiary}
